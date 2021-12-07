a2a1650605b7ef324462bd4d100de07403fa3f19
Aditya Desai <apd10@rice.edu>
2021-12-06 23:06:15 -0600

    With a lot of prints. But working code

--- LMAEmbedding.py
+++ LMAEmbedding.py
@@ -22,7 +22,8 @@ def generateSparseSRP(N, d):
 class LMAEmbeddingFunction(torch.autograd.Function):
     @staticmethod
 
-    def forward(ctx, hashed_weights, input_embeddings, lsh_matrix, random_numbers, input_dim, embedding_dim, num_chunks, chunk_size, bits_per_chunk, lsh_mode):
+    def forward(ctx, hashed_weights, input_embeddings, lsh_matrix, random_numbers, input_dim, embedding_dim,
+                    num_rep, num_chunks, chunk_size, bits_per_chunk, lsh_mode):
         '''
             read a chunk_size by performing lsh according to the lsh_mode,
             join chunks to create an embedding of size embedding_dim for each of the
@@ -35,11 +36,13 @@ class LMAEmbeddingFunction(torch.autograd.Function):
         
         
         output, hashed_idx = \
-            lma_embedding_bag.forward(hashed_weights, input_embeddings, lsh_matrix, random_numbers,  input_dim, embedding_dim, num_chunks, chunk_size, bits_per_chunk, lsh_mode_enum)
+            lma_embedding_bag.forward(hashed_weights, input_embeddings, lsh_matrix, random_numbers,  input_dim, embedding_dim, num_rep, num_chunks, chunk_size, bits_per_chunk, lsh_mode_enum)
+
         
         ctx.save_for_backward(hashed_idx)
         ctx.hashed_weights_size = hashed_weights.shape[0]
-        return output
+        return hashed_idx, output
+
     '''
     @staticmethod
     def backward(ctx, grad):
@@ -51,20 +54,22 @@ class LMAEmbeddingFunction(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad):
-        hashed_idx = ctx.saved_variables
+        hashed_idx, = ctx.saved_variables
         hashed_weights_size = ctx.hashed_weights_size
-        if hashed_idx.is_contiguous():
-            hashed_idx1 = hashed_idx.view(-1)
-        else:
-            hashed_idx1 = hashed_idx.reshape(-1)
+
+        weight_grad = torch.zeros(hashed_weights_size).to(grad.device)
+
         if grad.is_contiguous():
             grad1 = grad.view(-1)
         else:
             grad1 = grad.reshape(-1)
+      
+        for i in range(hashed_idx.shape[1]):
+            hashed_idx1 = hashed_idx[:,i,:].reshape(-1)
+            weight_grad.scatter_add_(0, hashed_idx1, grad1)
+        weight_grad = weight_grad / hashed_idx.shape[1]
 
-        weight_grad = torch.zeros(hashed_weights_size).to(grad.device)
-        weight_grad.scatter_add_(0, hashed_idx1, grad1)
-        return weight_grad, None, None, None, None, None, None, None, None
+        return weight_grad, None, None, None, None, None, None, None, None, None, None
 
 class LMAEmbedding(nn.Module):
     def __init__(
@@ -73,26 +78,37 @@ class LMAEmbedding(nn.Module):
         embedding_dim: int, 
         chunk_size: int,
         bits_per_chunk: int,
+        num_rep: int,
         hashed_weight: torch.Tensor,
         lsh_mode = "srp",
         seed = 1024)->None:
         super(LMAEmbedding, self).__init__()
+
 
         self.embedding_dim = embedding_dim
         self.lsh_mode = lsh_mode
         self.chunk_size = chunk_size
         self.bits_per_chunk = bits_per_chunk
         self.input_dim = input_dim
+        self.num_rep = num_rep
         self.weight = hashed_weight
-
-        assert(embedding_dim % chunk_size == 0)
+        self.memory_size = hashed_weight.size(0)
+        self.array_size = self.memory_size / num_rep
         self.num_chunks = int(embedding_dim / chunk_size)
 
+        assert(embedding_dim % chunk_size == 0)
+        assert(self.memory_size % num_rep == 0)
+
+
+        print("[single]Potential Memory Usage: ", 2**self.bits_per_chunk * self.num_chunks,  
+              "memory available", self.array_size)
+        print("[total ]Potential Memory Usage: ", 2**self.bits_per_chunk * self.num_chunks * self.num_rep,  
+              "memory available", self.memory_size)
+
         if (lsh_mode == "srp"):
-            self.lsh_matrix = Parameter(torch.from_numpy(generateSparseSRP(input_dim, self.num_chunks * self.bits_per_chunk).astype(np.float32)), requires_grad=False)
+            self.lsh_matrix = Parameter(torch.from_numpy(generateSparseSRP(input_dim, self.num_chunks * self.bits_per_chunk * self.num_rep).astype(np.float32).reshape(input_dim, self.num_rep, self.num_chunks * self.bits_per_chunk)), requires_grad=False)
         else:
             raise ValueError("No lsh mode found")
-
 
         r = np.random.RandomState(seed)
         # first number is the prime, rest are random integers
@@ -100,8 +116,9 @@ class LMAEmbedding(nn.Module):
         self.random_numbers = Parameter(torch.from_numpy(random_numbers.astype(np.int64)), requires_grad=False)
         print("RandomNumbers: ", self.random_numbers[:5])
         
-        print("LMAEmbedding: input_dim: ", input_dim, " embedding_dim:", embedding_dim, " chunk_size:",
-              chunk_size, " bits_per_chunk:", bits_per_chunk, "lsh_mode:", lsh_mode, " seed:", seed, " lsh matrix", self.lsh_matrix.shape)
+        print("LMAEmbedding: input_dim: ", input_dim, " embedding_dim:", embedding_dim, "rep:", self.num_rep, "chunk_size:",
+              chunk_size, " bits_per_chunk:", bits_per_chunk, "lsh_mode:", lsh_mode, " seed:", seed, 
+              " lsh matrix", self.lsh_matrix.shape,  "memory_size:", self.memory_size, "array_size:", self.array_size)
 
     def forward(self, input_embeddings) -> torch.Tensor:
         #i_shape = indices.shape
@@ -110,6 +127,7 @@ class LMAEmbedding(nn.Module):
         #    offsets  = torch.arange(len(indices)).to(indices.device)
         #assert(per_sample_weights is None)
         embeddings =  LMAEmbeddingFunction.apply(
-            self.weight, input_embeddings, self.lsh_matrix, self.random_numbers,  self.input_dim, self.embedding_dim, self.num_chunks, self.chunk_size, self.bits_per_chunk, self.lsh_mode
+            self.weight, input_embeddings, self.lsh_matrix, self.random_numbers,  self.input_dim, self.embedding_dim, self.num_rep, self.num_chunks, self.chunk_size, self.bits_per_chunk, self.lsh_mode
         )
         return embeddings
+
--- lma_embedding_bag_kernel.cu
+++ lma_embedding_bag_kernel.cu
@@ -18,22 +18,28 @@
 #include <stdio.h>
 
 #define MAX_GRID_SIZE  2048
-#define MAX_BLOCK_SIZE  512
+#define MAX_BLOCK_SIZE  128
 
 
 __device__ int64_t hash_func(int64_t a, int64_t b, const torch::PackedTensorAccessor32<int64_t, 1, torch::RestrictPtrTraits> random_numbers) {
     return (a * random_numbers[3] + b * random_numbers[2] + random_numbers[1]) % random_numbers[0];
 }
 
+inline __device__ int location2d(int i,int j, int I, int J) {
+    return i * J + j; // rowmajor
+}
+
 template<typename scalar_t>
 __global__ void lma_embedding_bag_update_output_kernel(
-    const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> random_projections,
+    const torch::PackedTensorAccessor32<scalar_t, 3, torch::RestrictPtrTraits> random_projections,
     const torch::PackedTensorAccessor32<scalar_t, 1, torch::RestrictPtrTraits> hashed_weights,
     torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits> output,
-    torch::PackedTensorAccessor32<int64_t, 2, torch::RestrictPtrTraits> hashed_index,
+    torch::PackedTensorAccessor32<int64_t, 3, torch::RestrictPtrTraits> hashed_index,
     const torch::PackedTensorAccessor32<int64_t, 1, torch::RestrictPtrTraits> random_numbers,
     int batch_size,
+    int array_size,
     int embedding_dim,
+    int num_rep,
     int num_chunks,
     int bits_per_chunk,
     int chunk_size,
@@ -41,41 +47,81 @@ __global__ void lma_embedding_bag_update_output_kernel(
     int lsh_mode_enum)
 {
     
-    int x_idx = blockIdx.x;
-    int y_idx = threadIdx.y;
-    if(x_idx == 0 and y_idx ==0) {
-      printf("bits_per_chunk: %d\n", bits_per_chunk);
-    }   
+    int x_idx = blockIdx.x; // for sample id  - need looping
+    int y_idx = threadIdx.y; // for chunk id - need looping
+    int z_idx = threadIdx.z; // repetition number - no looping
     
-    int64_t location = 0;
-    int64_t power2 = 1;
-    int bit_offset = 0;
-    int chunk_offset = 0; 
-    int64_t idx;
-    int64_t final_idx;
-    for(int i=x_idx; i < batch_size; i+= gridDim.x) {
+    //printf("x_idx: %d, y_idx: %d, z_idx:%d  blockdimensions:(%d,%d,%d) griddim:(%d,%d,%d)\n", 
+    //        x_idx, y_idx, z_idx, blockDim.x, blockDim.y, blockDim.z, gridDim.x, gridDim.y, gridDim.z);
+    if ( x_idx < batch_size and y_idx < num_chunks and z_idx < num_rep) {
+      int64_t location = 0;
+      int64_t power2 = 1;
+      int bit_offset = 0;
+      int chunk_offset = 0; 
+      int local_chunk_offset = 0;
+      int local_max_total_chunk = chunk_size * blockDim.y;
+      int64_t idx;
+      int64_t final_idx;
+      int stride;
+      int num;
+      // for per block. blockDim.x=1 * blockDim.y * blockDim.z * chunk_size
+      extern __shared__ float local_output[];
+      bool even;
+      for(int i=x_idx; i < batch_size; i+= gridDim.x) {
         for(int j = y_idx; j < num_chunks; j+= blockDim.y) {
+          printf("example: %d(+%d) chunk_id:%d(+%d) repetition:%d\n", i, gridDim.x, j, blockDim.y, z_idx);
+
           // i is the example id
           // j is the chunk id
           location = 0;
           power2 = 1;
           bit_offset = bits_per_chunk * j;
           for(int k = 0; k < bits_per_chunk ; k++) {
-              location += ((random_projections[i][bit_offset + k] > 0) ? power2 : 0);
-              power2 = power2<<1;
-              printf("RP: %d %d %f \n",i, bit_offset + k, random_projections[i][bit_offset + k]);
+            //location += ((random_projections[i][z_idx][bit_offset + k] > 0) ? power2 : 0);
+            if (random_projections[i][z_idx][bit_offset + k] > 0) {
+              location = location + power2;
+              printf("[%d,%d,%d] power:%ld location:%ld\n", i,j,z_idx, power2,location);
+            }
+            printf("[%d,%d,%d] loop : using projection: (%d,%d,%d) value: %f bit:%d inchunk:%d power2:%ld location:%ld\n", i,j,z_idx, i, z_idx, bit_offset + k, random_projections[i][z_idx][bit_offset + k], (random_projections[i][z_idx][bit_offset + k] > 0), k, power2, location);
+
+            power2 = power2<<1L;
           }
-
-          idx = hash_func(location, j, random_numbers) % hashedWeightSize;
+          idx = hash_func(location, j, random_numbers) % array_size;
+          printf("[%d,%d,%d] location: %ld idx: %ld\n", i,j,z_idx, location, idx);
           chunk_offset = chunk_size * j;
+          local_chunk_offset = chunk_size * (j % blockDim.y); // only need to look at the block
 
-          printf("x_idx:%d y_idx:%d i:%d j:%d loc:%ld idx:%ld \n", x_idx,y_idx,i,j, location, idx);
+          //printf("x_idx:%d z_idx:%d i:%d j:%d loc:%ld idx:%ld \n", x_idx,z_idx,i,j, location, idx);
           for(int k=0; k < chunk_size ; k++) {
-              final_idx = (idx + k) % hashedWeightSize;
-              hashed_index[i][chunk_offset + k] = final_idx;
-              output[i][chunk_offset + k] = hashed_weights[final_idx];
+            final_idx = z_idx * array_size + (idx + k) % array_size;
+            hashed_index[i][z_idx][chunk_offset + k] = final_idx;
+            local_output[location2d(z_idx,local_chunk_offset+ k,num_rep, local_max_total_chunk)] = hashed_weights[final_idx];
+            printf("[%d,%d,%d] idx: %ld final_idx: %ld hashed_weight: %f \n", i,j,z_idx, idx, final_idx, hashed_weights[final_idx]);
+          }
+          // need to reduce local_output into output 
+          num = num_rep;
+          while (num > 1) {
+            even = ((num & 1) == 0);
+            stride =  num / 2;
+            if( z_idx < stride) {
+              for(int k=0; k < chunk_size ; k++) {
+                local_output[location2d(z_idx, local_chunk_offset + k, num_rep, local_max_total_chunk)] += local_output[location2d(z_idx+stride, local_chunk_offset + k, num_rep, local_max_total_chunk)];
+              }
+            }
+            num = stride + (even ? 0 : 1);
+            if(i == 0 and j==0 and z_idx == 0) {
+              printf("i,j:(%d,%d) : num: %d stride:%d even:%d\n", i, j, num, stride, even);
+            }
+            __syncthreads(); // sync block
+          }
+          if(z_idx == 0) {
+            for(int k=0; k < chunk_size ; k++) {
+              output[i][chunk_offset + k] = local_output[location2d(z_idx, local_chunk_offset + k, num_rep, local_max_total_chunk)] / num_rep;
+              printf("[%d,%d,%d] ouput: %f\n",  i,j,z_idx, output[i][chunk_offset + k]);
+            }
           }
         }
+      }
     }
 }
 
@@ -83,22 +129,25 @@ __global__ void lma_embedding_bag_update_output_kernel(
 std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_cuda_forward(
     const torch::Tensor& hashed_weights, // 1 x n
     const torch::Tensor& input_embeddings, // b x d1
-    const torch::Tensor& lsh_matrix, // d1 x num_chunks
+    const torch::Tensor& lsh_matrix, // d1 x num_rep x chunks
     const torch::Tensor& random_numbers,
     int input_dim,
     int embedding_dim,
+    int num_rep,
     int num_chunks,
     int chunk_size,
     int bits_per_chunk,
     int lsh_mode_enum
-)
+    )
 {
 
-    auto random_projections = at::mm(input_embeddings, lsh_matrix); // b x (num_chunks*bits_per_chunk)
+  auto lsh_matrix_flat = at::reshape(lsh_matrix, {input_embeddings.size(1), -1});
+  auto random_projections = at::matmul(input_embeddings, lsh_matrix_flat); // b x num_rep x (num_chunks*bits_per_chunk)
+  random_projections = at::reshape(random_projections, {input_embeddings.size(0), num_rep, -1});
     cudaDeviceSynchronize();
 
     int64_t hashedWeightSize = hashed_weights.size(0);
-    auto hashed_index = at::empty({input_embeddings.size(0), embedding_dim}, input_embeddings.options().dtype(torch::kInt64)); // everything is same but it is int64 and not float
+    auto hashed_index = at::empty({input_embeddings.size(0), num_rep, embedding_dim}, input_embeddings.options().dtype(torch::kInt64)); // everything is same but it is int64 and not float
     auto output = at::empty({input_embeddings.size(0), embedding_dim}, input_embeddings.options());
     cudaStream_t stream = at::cuda::getCurrentCUDAStream(input_embeddings.device().index()); 
 
@@ -108,11 +157,15 @@ std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_cuda_forward(
     dim3 block = dim3(32, 8);
 #endif
     int grid = 1024;
-
-    if (num_chunks <= MAX_BLOCK_SIZE) {
-        block = dim3(1,num_chunks);
+    if( num_rep > 64) {
+      printf("Too many repetitions not supported, max: %d\n", MAX_BLOCK_SIZE);
+      assert(false);
+    }
+    int max_chunks = (int)((MAX_BLOCK_SIZE + num_rep - 1) /num_rep);
+    if (num_chunks <= max_chunks) {
+        block = dim3(1, num_chunks, num_rep);
     } else {
-        block = dim3(1, MAX_BLOCK_SIZE);
+        block = dim3(1, max_chunks, num_rep);
     }
     
     if (input_embeddings.size(0) < MAX_GRID_SIZE) {
@@ -120,22 +173,26 @@ std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_cuda_forward(
     } else {
         grid = MAX_GRID_SIZE;
     }
-    printf("Calling the kernel\n"); fflush(stdout);
+
+    printf("Calling the kernel grid:%d block:(1,%d,%d) \n", grid, num_chunks, num_rep); fflush(stdout);
     AT_DISPATCH_FLOATING_TYPES(hashed_weights.type(), "lma_embedding_bag_cuda", ([&] {
-        lma_embedding_bag_update_output_kernel<scalar_t><<<grid, block, 0, stream>>>(
-            random_projections.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
+        lma_embedding_bag_update_output_kernel<scalar_t><<<grid, block, (num_rep * block.y * chunk_size * sizeof(float)), stream>>>(
+            random_projections.packed_accessor32<scalar_t, 3, torch::RestrictPtrTraits>(),
             hashed_weights.packed_accessor32<scalar_t, 1, torch::RestrictPtrTraits>(),
             output.packed_accessor32<scalar_t, 2, torch::RestrictPtrTraits>(),
-            hashed_index.packed_accessor32<int64_t, 2, torch::RestrictPtrTraits>(),
+            hashed_index.packed_accessor32<int64_t, 3, torch::RestrictPtrTraits>(),
             random_numbers.packed_accessor32<int64_t, 1, torch::RestrictPtrTraits>(),
             random_projections.size(0),
+            (int) hashed_weights.size(0) / num_rep,
             embedding_dim,
+            num_rep,
             num_chunks,
             bits_per_chunk,
             chunk_size,
             hashedWeightSize,
             lsh_mode_enum);
     }));
+    fflush(stdout);
     cudaDeviceSynchronize();  
    return std::tuple<torch::Tensor, torch::Tensor>(
         output, hashed_index);
@@ -148,9 +205,6 @@ std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_cuda_forward(
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 
-
-//lma_embedding_bag.forward(hashed_weights, input_embeddings, lsh_matrix, input_dim, embedding_dim, num_chunks, chunk_size, bits_per_chunk, lsh_mode)
-        
 std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_forward(
     const torch::Tensor& hashed_weights,
     const torch::Tensor& input_embeddings,
@@ -158,6 +212,7 @@ std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_forward(
     const torch::Tensor& random_numbers,
     int input_dim,
     int embedding_dim,
+    int num_rep,
     int num_chunks,
     int chunk_size,
     int bits_per_chunk,
@@ -170,9 +225,9 @@ std::tuple<torch::Tensor, torch::Tensor> lma_embedding_bag_forward(
     CHECK_INPUT(lsh_matrix);
     CHECK_INPUT(random_numbers);
     
-
     return lma_embedding_bag_cuda_forward( hashed_weights, input_embeddings, lsh_matrix, random_numbers,
-                                          input_dim, embedding_dim, num_chunks, chunk_size, bits_per_chunk, lsh_mode_enum);
+                                          input_dim, embedding_dim, num_rep, num_chunks, chunk_size, 
+                                          bits_per_chunk, lsh_mode_enum);
 }
 
 
